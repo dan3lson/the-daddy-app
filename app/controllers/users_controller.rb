9bb8a3c8d22881ee0bf9b4ce6504166e3454feb1
Danelson Rosa <danelson.rosa@gmail.com>
2019-11-25 06:35:07 -0500

    Add and style sign up and sign in errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < Clearance::UsersController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to signed_in_root_path
+    else
+      render template: 'users/new'
+    end
+  end
+
   private
 
   def user_params
