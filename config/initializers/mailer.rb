f3e4e52f16c98c6c5607271fb2f180e26889dc97
Danelson Rosa <danelson.rosa@gmail.com>
2022-10-01 15:04:55 -0400

    Open sent emails locally

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,5 +3,4 @@ Rails.application.configure do
   config.action_mailer.asset_host = "#{ENV['PROTOCOL']}://#{ENV['BASE_URL']}"
   config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
   config.serve_static_assets = true
-  config.active_job.queue_adapter = :sidekiq
 end
