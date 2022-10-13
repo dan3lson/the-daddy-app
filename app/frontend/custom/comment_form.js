b42653651a51b01217b5e169989ce14de51c1004
Danelson Rosa <danelson.rosa@gmail.com>
2022-10-12 20:31:10 -0400

    Comment out comment_form.js
    Was causing a console error. Will revisit later :fingers-crossed:

--- app/frontend/custom/comment_form.js
+++ app/frontend/custom/comment_form.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
-    const fileInput = document.querySelector('#file-js-example input[type=file]');
-    fileInput.onchange = () => {
-        if (fileInput.files.length > 0) {
-            const fileName = document.querySelector('#file-js-example .file-name');
-            fileName.textContent = fileInput.files[0].name;
-        }
-    }
-});
+// Commented out because it's throwing a console warning (likely due to the form not rendered
+// on certain pages).
+//
+// $(function() {
+//     const fileInput = document.querySelector('#file-js-example input[type=file]');
+//     fileInput.onchange = () => {
+//         if (fileInput.files.length > 0) {
+//             const fileName = document.querySelector('#file-js-example .file-name');
+//             fileName.textContent = fileInput.files[0].name;
+//         }
+//     }
+// });
