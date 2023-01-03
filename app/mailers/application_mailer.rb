e98ef31bae0c30fd1282b13162d24f8637e564fa
Danelson Rosa <gitdanelson@gmail.com>
2023-01-03 00:30:11 -0500

    Fix ApplicationMailer from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@
 
 # Class to represent the default mailer
 class ApplicationMailer < ActionMailer::Base
-  default from: '<The Daddy App> hello@thedaddyapp.com'
+  default from: 'The Daddy App <hello@thedaddyapp.com>'
   layout 'mailer'
 end
