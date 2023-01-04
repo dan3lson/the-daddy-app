bb84455684a8032ef96d255e941636ab62e4707a
Danelson Rosa <gitdanelson@gmail.com>
2023-01-04 13:03:16 -0500

    Define who a First-timer and Juggler are

--- app/services/lib/constants.rb
+++ app/services/lib/constants.rb
@@ -3,12 +3,12 @@
 module Lib
   module Constants
     DADDY_TYPES = {
-      "The First-timer": "first_timer",
-      "The Juggler":     "juggler",
-      "The Stepdad":     "stepdad",
-      "The Adopter":     "adopter",
-      "The Fosterer":    "fosterer",
-      "Other":           "other",
+      "The First-timer (one child)": 'first_timer',
+      "The Juggler (multiple children)": 'juggler',
+      "The Stepdad": 'stepdad',
+      "The Adopter": 'adopter',
+      "The Fosterer": 'fosterer',
+      "Other": 'other'
     }
   end
 end
