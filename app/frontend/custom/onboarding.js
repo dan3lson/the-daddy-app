d104d99235469f90037283ac701bd92a1c435ad8
Danelson Rosa <gitdanelson@gmail.com>
2023-01-23 22:38:47 -0500

    Fix referral-section skip button

--- app/frontend/custom/onboarding.js
+++ app/frontend/custom/onboarding.js
@@ -1,7 +1,7 @@
 $(function() {
 	$(".onboarding-next-btn").on("click", function(e) {
 		e.preventDefault();
-		$section = $(this).parent();
+		$section = $(this).parents("section");
 		$section.addClass("d-none");
 		$section.next().removeClass("d-none");
 		$section.next().hide().fadeIn();
