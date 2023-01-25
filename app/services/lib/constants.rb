9f47701094a74bc3fd9681943e3f25908458ea18
Danelson Rosa <gitdanelson@gmail.com>
2023-01-25 12:20:59 -0500

    Make first round of visual updates

--- app/services/lib/constants.rb
+++ app/services/lib/constants.rb
@@ -9,6 +9,21 @@ module Lib
       "The Adopter": "adopter",
       "The Fosterer": "fosterer",
       Other: "other"
-    }
+    }.freeze
+    public_constant :DADDY_TYPES
+
+    CREATE_COMMENT_EXAMPLES = [
+      "My little man just crawled!!",
+      "Just saying hello 👋",
+      "But why does my 8/mo daughter love pooping when I give her a bath 😩"
+    ].freeze
+    public_constant :CREATE_COMMENT_EXAMPLES
+
+    CREATE_COMMENT_LABEL_EXAMPLES = [
+      "How's it going?",
+      "What's on your mind?"
+      # "What's up with your little one(s)?"
+    ].freeze
+    public_constant :CREATE_COMMENT_EXAMPLES
   end
 end
