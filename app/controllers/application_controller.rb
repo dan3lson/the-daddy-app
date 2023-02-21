df019978c5810da2263025bcd6305be5223ad0bb
Danelson Rosa <gitdanelson@gmail.com>
2023-02-21 06:36:10 -0500

    Track all controller actions
    Will filter it to more specific ones later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  after_action :track_action
+
+  protected
+
+  def track_action
+    ahoy.track "Ran action", request.path_parameters
+  end
 end
