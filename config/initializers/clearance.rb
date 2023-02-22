03f4656364382fd9719708cd46b073ba96286fe6
Danelson Rosa <gitdanelson@gmail.com>
2023-02-22 17:02:24 -0500

    Set Clearance signup config to default

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Clearance.configure do |config|
+  config.allow_sign_up = true
   config.routes = false
   config.mailer_sender = "hello@thedaddyapp.com"
   config.rotate_csrf_on_sign_in = true
