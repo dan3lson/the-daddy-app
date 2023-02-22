b3437cf0760f358add8958064d54270b99a521fa
Danelson Rosa <gitdanelson@gmail.com>
2023-02-22 15:19:55 -0500

    Update image helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,6 @@ module ApplicationHelper
   end
 
   def img(filename, description, options = {})
-    image_tag(filename, alt: description, title: description, **options)
-  end
-
-  def img_pack(filename, description, options = {})
     image_pack_tag(filename, alt: description, title: description, **options)
   end
 
