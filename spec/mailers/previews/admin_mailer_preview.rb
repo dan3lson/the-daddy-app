6c728c5fcb10a1016321f6396531ad30a4a38aca
Danelson Rosa <gitdanelson@gmail.com>
2023-02-24 07:23:22 -0500

    Update mailer footers and bg color

--- spec/mailers/previews/admin_mailer_preview.rb
+++ spec/mailers/previews/admin_mailer_preview.rb
@@ -1,6 +1,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers
 class AdminMailerPreview < ActionMailer::Preview
   def alert
-    AdminMailer.alert(additional_data: {}, current_user_id: nil, message: "yikes, something went wrong", type: "error")
+    AdminMailer.alert(
+      additional_data: {},
+      current_user_id: User.first,
+      message: "A new user has signed up!",
+      type: "info"
+    )
   end
 end
