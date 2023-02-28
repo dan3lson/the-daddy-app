b85c0e2d204a71e42669f54fb42df4f26d221b4b
Danelson Rosa <gitdanelson@gmail.com>
2023-02-27 22:36:11 -0500

    Remove one-off mailers

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -21,26 +21,4 @@ class UserMailerPreview < ActionMailer::Preview
   def reply
     UserMailer.reply(Comment.first.id)
   end
-
-  def waitlist_user_joined
-    UserMailer.waitlist_user_joined(
-      email: "some_excited_daddy@email.com",
-      first_name: "Danelson Sr.",
-      referral_code: "s0meCod3"
-    )
-  end
-
-  # == One-off Emails
-  #
-
-  def update_waitlist_users_before_alpha_launch
-    UserMailer.update_waitlist_users_before_alpha_launch(WaitlistUser.first)
-  end
-
-  def alpha_release_now_live
-    UserMailer.alpha_release_now_live(
-      email: "danelson.rosa.sr@gmail.com",
-      first_name: "Danelson Sr."
-    )
-  end
 end
